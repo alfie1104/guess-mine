88fd4a882b60938a99806ef4a23047b9c7a7142f
peter_alfie <alfie1104@gmail.com>
2019-12-05 09:00:51 +0900

    players and notif part are done

--- assets/js/players.js
+++ assets/js/players.js
@@ -1,1 +1,44 @@
-export const handlePlayerUpdate = ({ sockets }) => console.log(sockets);
+import {
+  disableCanvas,
+  hideControls,
+  enableCanvas,
+  showControls,
+  resetCanvas
+} from "./paint";
+
+const board = document.getElementById("jsPBoard");
+const notifs = document.getElementById("jsNotifs");
+
+const addPlayers = players => {
+  board.innerHTML = "";
+  players.forEach(player => {
+    const playerElement = document.createElement("span");
+    playerElement.innerText = `${player.nickname}: ${player.points}`;
+    board.appendChild(playerElement);
+  });
+};
+
+const setNotifs = text => {
+  notifs.innerText = "";
+  notifs.innerText = text;
+};
+
+export const handlePlayerUpdate = ({ sockets }) => addPlayers(sockets);
+export const handleGameStarted = () => {
+  setNotifs("");
+  disableCanvas();
+  hideControls();
+};
+
+export const handleLeaderNotif = ({ word }) => {
+  enableCanvas();
+  showControls();
+  setNotifs(`You are the leader, paint: ${word}`);
+};
+
+export const handleGameEnded = () => {
+  setNotifs("Game ended.");
+  disableCanvas();
+  hideControls();
+  resetCanvas();
+};
